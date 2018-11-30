73d22ce9b87c78b9d2b41c1109b033743335e1c2
hadiiiiii <45215673+hadiiiiii@users.noreply.github.com>
2018-11-29 22:33:32 -0800

    Create dndn.js

---
+++ dndn.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');//????? ???
+const client = new Discord.Client();// ????? ???????
+const ms = require('ms');
+const pms = require('pretty-ms');
+
+
+const CodingXActivity = [
+  "minecraft", //IT's Broken Here
+  "PUBG",
+  ];
+  
+client.on('ready', () => {
+  setInterval(() => {
+      const index = Math.floor(Math.random() * (CodingXActivity.length - 1) + 1);
+ client.user.setGame(CodingXActivity[index], "https://www.twitch.tv/CodingXArea");
+  }, 10000); //10 seconds
+});
